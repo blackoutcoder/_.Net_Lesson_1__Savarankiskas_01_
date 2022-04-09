0d6f382c8b09d0ebcd98c80c29670a5368acbcdc
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-04-09 23:29:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _.Net_Lesson_1__Savarankiskas_01_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kokia tavo megstamiausia spalva?");
+            string spalva = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Mano irgi {spalva}. :)");
+            Console.ReadLine();
+        }
+    }
+}
